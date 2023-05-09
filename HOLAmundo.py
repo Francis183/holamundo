681d08348413edcecf2c6f4778cecb010a8b3195
Francis <escore163@gmail.com>
2023-05-09 17:57:54 -0500

    Prueba

---
+++ HOLAmundo.py
@@ -0,0 +1,6 @@
+altura = float(input("Por favor ingrese su altura (en cm): "))
+peso = float(input("Por favor ingrese su peso (en kg): "))
+    
+bmi = peso / ((altura/100) * (altura/100))
+
+print("Su Índice de Masa Corporal es:", bmi)
